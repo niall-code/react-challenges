ba60a55c9f8e45c3983fdec5017f29901f19dc5c
Niall Caplis <niallcaplis@gmail.com>
2024-08-29 13:07:32 +0000

    add Sidebar functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import css from './App.module.css';
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-    </div>
-  );
+    return (
+        <div className={css.App}>
+            <Sidebar />
+        </div>
+    );
 }
 
 export default App;
