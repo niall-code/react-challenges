48f51280144a7cde8517d1ea3fdc4096adbaf248
Niall Caplis <niallcaplis@gmail.com>
2024-09-01 18:11:16 +0000

    add NavBarSimple.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import css from "./App.module.css";
+import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 
 function App() {
     return (
         <div className={css.App}>
+            <NavBarSimple />
             <Sidebar />
         </div>
     );
