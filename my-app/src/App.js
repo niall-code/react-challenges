5e87033fbdb0250762ef49591d7a953aa16d9287
Niall Caplis <niallcaplis@gmail.com>
2024-09-02 19:00:11 +0000

    include NavBarForm and NavBarChild

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from "./App.module.css";
-import NavBarSimple from "./components/NavBarSimple";
+import NavBarForm from "./components/NavBarForm";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 
 function App() {
     return (
         <div className={css.App}>
-            <NavBarSimple />
+            {/* <NavBarSimple /> */}
+            <NavBarForm />
             <Sidebar />
         </div>
     );
