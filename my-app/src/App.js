ec5b3d7ea8121f93ed36bfe8683d3e2743ae3814
Niall Caplis <niallcaplis@gmail.com>
2024-09-06 13:24:00 +0000

    include list-rendering challenge solution

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import css from "./App.module.css";
+import Content from "./components/Content";
 import NavBarForm from "./components/NavBarForm";
-// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 
 function App() {
     return (
         <div className={css.App}>
-            {/* <NavBarSimple /> */}
             <NavBarForm />
             <Sidebar />
+            <Content />
         </div>
     );
 }
