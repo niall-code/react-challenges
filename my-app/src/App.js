2f54ced62c5fed3858e843c9b8a004f6e8c311f3
Niall Caplis <niallcaplis@gmail.com>
2024-09-17 13:49:52 +0000

    combining axios API requests and React hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import css from "./App.module.css";
 import Content from "./components/Content";
 import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 import ContentHooks from "./components/ContentHooks";
 import NavBarForm from "./components/NavBarForm";
 import Sidebar from "./components/Sidebar";
@@ -12,7 +13,8 @@ function App() {
             <Sidebar />
             {/* <Content /> */}
             {/* <ContentHooks /> */}
-            <ContentAPI />
+            {/* <ContentAPI /> */}
+            <ContentAPIHooks />
         </div>
     );
 }
